455729db6083b5cddc32fb95c1933ac8706bc131
Daniel Duan <daniel@duan.ca>
2020-08-27 20:09:10 -0700

    Initial Commit

---
+++ CoverMaker/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  CoverMaker
+//
+//  Created by Daniel Duan on 8/27/20.
+//  Copyright © 2020 Daniel Duan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
